d5066bc8c162511ded2a73fea2d2516f718572be
elenatafeenko <elenatafeenko@gmail.com>
2017-03-11 21:58:32 +0300

    header done: upper blok + nav (without dropdown)

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,24 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var sourceMaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('less', function() {
   return gulp.src('./less/application.less')
     .pipe(sourceMaps.init())
     .pipe(less())
+    .pipe(postcss([ autoprefixer() ]))
     .pipe(sourceMaps.write())
     .pipe(gulp.dest('./public'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', ['less'], function() {
   return watch('less/**/*.less', function () {
     gulp.src('./less/application.less')
       .pipe(sourceMaps.init())
       .pipe(less())
+      .pipe(postcss([ autoprefixer() ]))
       .pipe(sourceMaps.write())
       .pipe(gulp.dest('./public'));
   });
